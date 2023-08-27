2c967bfaf28f99fcf1a92f1d2e893ac95c22cdbc
alina513 <alinaromanko513@gmail.com>
2023-08-27 16:01:53 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems.map(({preview, original, description}) =>
+{return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img
+    class="gallery__image"src="${preview}"data-source="${original}"alt="${description}"
+    /></a></li>`})
+    .join("");
+console.dir(markup)
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", onClick);
+function onClick(event) {
+    event.preventDefault();
+    if(!event.target.classList.contains("gallery__image"))
+    {return;}
+    
+    const data = event.target.dataset.source;
+    const currentItem = galleryItems.find(({original}) => 
+         original === data
+         );
+         const instance = basicLightbox.create(`<div class="modal">
+         <img class="gallery__image" src="${currentItem.original}" alt="${currentItem.description}"
+/></div>`);
+console.log(instance)
+instance.show();
+    
+};
